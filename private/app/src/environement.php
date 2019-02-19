46e100d0d0715d99125ccb86a5a2ef2bc89c0c8b
vincent Robin <v.robin59@gmail.com>
2019-02-19 16:56:55 +0100

    -

---
+++ private/app/src/environement.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Fichier de définition de l'environement d'éxecution de l'application
+ * 
+ */
+
+// dans ke cas ou la variable $dev_domains n'est pas défini (dasn le fichier config.php)
+// on initialise la variable $dev_domains avec un tableau vide
+
+if(!isset($dev_domains)){
+    $dev_domains=[];
+}
+
+//si la super global $_SERVER["SERVERNAME"] n'est pas vide
+// et que la valeur de la super global $_SERVER["SERVERNAME"] est dans le tableau
+//$dev_domains, alors on redefini la variable $env
+
+// si la valeur de $_SERVER["SERVER_NAME"] n'est pas vide et $_SERVER["SERVER_NAME"] contenu dans le tableau $dev_domains on est en dev.
+if (!empty($_SERVER["SERVER_NAME"]) && in_array($_SERVER["SERVER_NAME"],$dev_domains) ) 
+    {
+        $env="DEV";
+    }
+
+
+echo "-------------------------<br>";
+echo "valeur de \$env =: ".$env;
+
+
